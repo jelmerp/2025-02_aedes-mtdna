4a16fa7e4b9abb6404bbc26b3f8c775fe05cb119
Jelmer Poelstra <poelstra.1@osu.edu>
2025-07-07 16:53:18 -0400

    Separate script for COI tree

--- scripts/COI-haps.R
+++ scripts/COI-haps.R
@@ -7,12 +7,10 @@
 library(tidyverse)
 library(adegenet)
 library(ape)
-library(ggtree)
-library(ips)
+library(ips) # Not working on OSC
 library(haplotypes)
 library(pegas)
 library(phytools)
-library(ggnewscale)
 
 # Settings
 pop_names <- c("Cleveland", "Medina", "Wooster")
@@ -23,13 +21,12 @@ nuc_cols <- c("rosybrown", "sienna1", "lightgoldenrod1", "lightskyblue1", "grey"
 options(ignore.negative.edge = TRUE)
 
 # Define input files
-fasta_file <- "results/COI_aln.fa"
-tree_file <- "results/COI_withrefs_aln.treefile"
-meta_refs_file <- "data/refs_meta.tsv"
-hap_xy_file <- "results/hapnetwork_coords.rds"
+fasta_file <- "results/COI/aligned/COI_aln.fa"
+tree_file <- "results/COI/iqtree/COI_withrefs_aln.treefile"
+hap_xy_file <- "results/COI/hapnetwork_coords.rds"
 
 # Define output files
-outdir <- "results"
+outdir <- "results/COI"
 dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
 network_file <- file.path(outdir, "haplotype_network.png")
 fst_file <- file.path(outdir, "fst.tsv")
@@ -38,13 +35,6 @@ fst_file <- file.path(outdir, "fst.tsv")
 # DATA PREP --------------------------------------------------------------------
 # Read the sequences
 seqs <- fasta2DNAbin(fasta_file)
-
-# Read the metadata
-meta_refs_init <- read_tsv(meta_refs_file, show_col_types = FALSE)
-
-# Read the tree
-iqtree <- read.tree(tree_file)
-iqtree <- ape::root(iqtree, outgroup = "HQ944971.1", resolve.root = TRUE)
 
 # Read the haplotype network coordinates
 xy <- readRDS(hap_xy_file)
@@ -67,87 +57,8 @@ rownames(seqs) <- rownames(seqs) |>
   gsub("_([0-9]+$)", ".\\1", x = _) |>
   gsub("^ON[0-9]+_", "", x = _)
 
-# Simplify sample labels in tree object
-iqtree$tip.label <- iqtree$tip.label |>
-  gsub("\\*+", "", x = _) |>
-  gsub("_([0-9]+$)", ".\\1", x = _) |>
-  gsub("^ON[0-9]+_", "", x = _) |>
-  sub("HQ944971.1", "HQ944971.1_Aedes canadensis", x = _) |>
-  sub("GQ254798.1", "GQ254798.1_Iowa_2008", x = _) |> 
-  sub("GQ254793.1_Iowa_USA.2007", "GQ254793.1_Illinois_2007", x = _) |>
-  sub("\\.(20[0-9]+)$", "_\\1", x = _)
-
 # Infer haplotypes
 kh <- haplotypes::haplotype(as.dna(seqs))
-
-# Prep metadata for ref sequences
-meta_refs <- meta_refs_init |>
-  mutate(
-    ID = sub("HQ944971.1", "HQ944971.1_Aedes canadensis", ID),
-    ID = sub("GQ254798.1", "GQ254798.1_Iowa_2008", ID), 
-    ID = sub("GQ254793.1_Iowa_USA.2007", "GQ254793.1_Illinois_2007", ID),
-    ID = sub("MW475673.1_Belgium.2019", "MW475673.1_Belgium_2019", ID),
-    Year = as.character(Year),
-    focal = FALSE,
-    Area = Location
-  )     
-
-# Prep metadata for own sequences and combine with ref
-meta <- tibble(full_id = names(unlist(kh@hapind))) |>
-  mutate(
-    ID = sub("haplotype[0-9]+\\.", "", full_id),
-    Haplotype = sub(".*haplotype([0-9]+).*", "\\1", full_id),
-    Location = sub("_.*", "", ID),
-    Area = "Ohio",
-    Year = sub(".*_([0-9]+)\\..*", "\\1", ID),
-    Species = "Aedes japonicus",
-    focal = TRUE
-  ) |>
-  select(-full_id) |>
-  bind_rows(meta_refs) 
-
-
-# RECTANGULAR TREE PLOT --------------------------------------------------------
-ggtree(iqtree, cex = 0.8) %<+% meta +
-  geom_tiplab(
-    aes(color = focal),
-    align = TRUE,
-    linesize = 0.25,
-    size = 2.6,
-    ) +
-  geom_tiplab(
-    geom = "label",
-    aes(label = Area, fill = Area),
-    offset = 0.3,
-    align = TRUE,
-    linesize = 0,
-    size = 2.1,
-    fontface = "bold"
-  ) +
-  scale_color_manual(values = c("grey70", "black"), na.translate = FALSE) +
-  scale_fill_brewer(palette = "Pastel2") +
-  guides(fill = "none", color = "none") +
-  geom_treescale(y = - 5, color = "coral4", fontsize = 4) +
-  coord_cartesian(clip = "off") +
-  theme(
-    legend.position = "top",
-    plot.margin = margin(0.5, 1, 0.05, 0.05, "cm"),
-    )
-
-ggsave("results/tree.png", width = 6, height = 8, dpi = 600)
-
-#p +
-#  new_scale_fill() +
-#  geom_tiplab(
-#    geom = "label",
-#    aes(label = Year, fill = Year),
-#    offset = 0.012,
-#    align = TRUE,
-#    linesize = 0,
-#    size = 2.3,
-#    fontface = "bold"
-#  ) +
-#  guides(fill = "none")
 
 
 # POPULATION-LEVEL HAPLOTYPE NETWORK -------------------------------------------
@@ -229,10 +140,12 @@ fst_CI <- hierfstat::boot.ppfst(gi)  # Get confidence intervals for FST values
 fst_ll <- data.frame(fst_CI$ll) |>
   rownames_to_column("pop1") |>
   pivot_longer(cols = !pop1, names_to = "pop2", values_to = "lower_limit")
+
 fst_final <- data.frame(fst_results) |>
   rownames_to_column("pop1") |>
   pivot_longer(cols = !pop1, names_to = "pop2", values_to = "FST") |>
   left_join(fst_ll, by = c("pop1", "pop2")) |>
   drop_na() |>
   mutate(is_significant = ifelse(test = lower_limit > 0, yes = TRUE, no = FALSE))
+
 write_tsv(fst_final, fst_file)
---
+++ scripts/COI-tree.R
@@ -0,0 +1,193 @@
+# 2025-07-06, Jelmer Poelstra at OSC-Cardinal, R 4.4.0
+
+# SETUP ------------------------------------------------------------------------
+# Load packages
+library(ape)
+library(tidyverse)
+library(ggtree)
+library(ggtreeExtra)
+
+# Settings
+outgroup <- "HQ944971.1"
+focal_locs <- c("Cleveland", "Medina", "Wooster", "other")
+loc_cols <- c("purple3", "chartreuse4", "goldenrod3", "grey80")
+
+# Define input files
+tree_file <- "results/COI/iqtree/COI_withrefs_aln.treefile"
+meta_refs_file <- "metadata/refs_meta.tsv"
+
+# Define output files
+outdir <- "results/plots"
+dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+plotfile_png <- file.path(outdir, "COI-tree.png")
+plotfile_svg <- file.path(outdir, "COI-tree.svg")
+plotfile_lab_png <- file.path(outdir, "COI-tree_labs.png")
+plotfile_lab_svg <- file.path(outdir, "COI-tree_labs.svg")
+plotfile_rect_png <- file.path(outdir, "COI-tree_rect.png")
+plotfile_rect_svg <- file.path(outdir, "COI-tree_rect.svg")
+
+# Read the input files
+meta_refs_init <- read_tsv(meta_refs_file, show_col_types = FALSE)
+tree <- read.tree(tree_file)
+
+# Set the outgroup
+tree <- ape::root(tree, outgroup = outgroup, resolve.root = TRUE)
+
+# Adjust edge length of outgroup because it is extremely long
+tree$edge.length[which.max(tree$edge.length)] <- 0.2
+
+
+# METADATA PREP ----------------------------------------------------------------
+# Prep tip labels
+tree$tip.label <- tree$tip.label |>
+  gsub("\\*+", "", x = _) |>
+  gsub("_([0-9]+$)", ".\\1", x = _) |>
+  gsub("^ON[0-9]+_", "", x = _) |>
+  sub("HQ944971.1", "HQ944971.1_Aedes canadensis", x = _) |>
+  sub("GQ254798.1", "GQ254798.1_Iowa_2008", x = _) |> 
+  sub("GQ254793.1_Iowa_USA.2007", "GQ254793.1_Illinois_2007", x = _) |>
+  sub("\\.(20[0-9]+)$", "_\\1", x = _)
+
+# Prep metadata for ref sequences
+meta_refs <- meta_refs_init |>
+  mutate(
+    ID = sub("HQ944971.1", "HQ944971.1_Aedes canadensis", ID),
+    ID = sub("GQ254798.1", "GQ254798.1_Iowa_2008", ID), 
+    ID = sub("GQ254793.1_Iowa_USA.2007", "GQ254793.1_Illinois_2007", ID),
+    plotlab = sub(".*\\.1_", "", ID),
+    plotlab = sub("Aedes canadensis", "(Aedes canadensis)", plotlab),
+    Year = as.character(Year),
+    is_focal = FALSE,
+    Area = Location
+  ) 
+
+# Prep metadata for own sequences
+meta_focal <- tibble(ID = tree$tip.label) |>
+  mutate(
+    Location = sub("_.*", "", ID),
+    Area = "Ohio",
+    Year = sub(".*_([0-9]+)\\..*", "\\1", ID),
+    Species = "Aedes japonicus",
+    is_focal = TRUE,
+    plotlab = ID,
+    plotlab = sub("\\.[0-9]+$", "", plotlab)
+  ) |>
+  filter(Location %in% focal_locs)
+
+# Combine  
+meta <- bind_rows(meta_refs, meta_focal) |>
+  mutate(
+    loc_focal = case_when(
+      grepl("Wooster", plotlab) ~ "Wooster",
+      grepl("Cleveland", plotlab) ~ "Cleveland",
+      grepl("Medina", plotlab) ~ "Medina",
+      .default = "other"
+    ),
+    loc_focal = factor(loc_focal, levels = focal_locs),
+    plotlab = sub("_", " ", plotlab)
+  )
+
+
+# PLOT -------------------------------------------------------------------------
+# Plot the tree - circular
+p <- ggtree(tree, size = 0.5, layout = "circular") %<+%
+  meta +
+  geom_tiplab(align = TRUE, linesize = 0.25, size = 0) +
+  geom_rootedge(rootedge = 0.005) +
+  geom_fruit(
+    geom = geom_tile,
+    mapping = aes(fill = loc_focal),
+    color = "black",
+    pwidth = 0.03,
+    width = 0.03
+  ) +
+  scale_fill_manual(values = loc_cols, name = "Location") +
+  theme(
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    legend.title = element_text(face = "bold")
+  )
+p
+
+ggsave(plotfile_png, width = 8, height = 7, dpi = 400)
+ggsave(plotfile_svg, width = 8, height = 7)
+
+# With labels
+p +
+  geom_tiplab(
+    aes(label = plotlab, color = is_focal),
+    align = TRUE,
+    linesize = 0,
+    size = 3,
+    offset = 0.03
+  ) +
+  scale_color_manual(values = c("grey70", "black"), na.translate = FALSE) +
+  guides(color = "none") +
+  theme(
+    legend.box.spacing = unit(50, "pt"),
+    plot.margin = margin(1.5, 1.5, 1.5, 1.5, "cm")
+  )
+
+ggsave(plotfile_lab_png, width = 8, height = 7, dpi = 400)
+ggsave(plotfile_lab_svg, width = 8, height = 7)
+
+
+# ALTERNATIVES -----------------------------------------------------------------
+# Plot the tree - rectangular
+ggtree(tree, size = 0.5, layout = "rectangular") %<+%
+  meta +
+  geom_tiplab(
+    aes(label = plotlab, color = loc_focal),
+    align = TRUE,
+    linesize = 0.2,
+    size = 3
+  ) +
+  geom_treescale(x = 0.01, y = length(tree$tip.label) - 3, color = "grey50") +
+  geom_rootedge(rootedge = 0.005) +
+  scale_color_manual(values = loc_cols) +
+  coord_cartesian(clip = "off") +
+  guides(color = "none") +
+  theme(plot.margin = margin(0.2, 5, 0.2, 0.2, "cm"))
+
+ggsave(plotfile_rect_png, width = 8, height = 7, dpi = 400)
+ggsave(plotfile_rect_svg, width = 8, height = 7)
+
+#p +
+#  new_scale_fill() +
+#  geom_tiplab(
+#    geom = "label",
+#    aes(label = Year, fill = Year),
+#    offset = 0.012,
+#    align = TRUE,
+#    linesize = 0,
+#    size = 2.3,
+#    fontface = "bold"
+#  ) +
+#  guides(fill = "none")
+
+# Alternative
+#ggtree(tree, cex = 0.8) %<+%
+#  meta +
+#  geom_tiplab(
+#    aes(color = focal),
+#    align = TRUE,
+#    linesize = 0.25,
+#    size = 2.6,
+#  ) +
+#  geom_tiplab(
+#    geom = "label",
+#    aes(label = Area, fill = Area),
+#    offset = 0.3,
+#    align = TRUE,
+#    linesize = 0,
+#    size = 2.1,
+#    fontface = "bold"
+#  ) +
+#  scale_color_manual(values = c("grey70", "black"), na.translate = FALSE) +
+#  scale_fill_brewer(palette = "Pastel2") +
+#  guides(fill = "none", color = "none") +
+#  geom_treescale(y = -5, color = "coral4", fontsize = 4) +
+#  coord_cartesian(clip = "off") +
+#  theme(
+#    legend.position = "top",
+#    plot.margin = margin(0.5, 1, 0.05, 0.05, "cm"),
+#  )
