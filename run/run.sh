0be45aad8c7684db5acce079067dd7cc713ad377
Jelmer Poelstra <poelstra.1@osu.edu>
2025-07-07 16:53:43 -0400

    Make NAD4 tree

--- run/run.sh
+++ run/run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# ==============================================================================
+#                               COI
+# ==============================================================================
+# Define input files
+ref_fa_init=/fs/ess/PAS0471/jelmer/assist/01_archive/2022-04_ferd_phylo/results/COIunalign_woutgroups.fa
+fa_init=data/Clean_COI_Sequences.fa 
+ref_ids=metadata/ref_ids.txt
+
+# Define output files
+outdir=results/COI && mkdir -p "$outdir"
+fa="$outdir"/fasta/COI.fa
+
+# Settings
+outgroup=HQ944971.1
+
 # Several sequences are of poor quality - remove these:
-seqkit grep -v -p 'Wooster_2024_14**' data/Clean_COI_Sequences.fa |
+seqkit grep -v -p 'Wooster_2024_14**' "$fa_init" |
     seqkit grep -v -p 'Medina_2023_4' |
     seqkit grep -v -p 'Medina_2023_2' |
     seqkit grep -v -p 'Medina_2023_3' |
@@ -11,27 +26,49 @@ seqkit grep -v -p 'Wooster_2024_14**' data/Clean_COI_Sequences.fa |
     seqkit grep -v -p 'Wooster_2024_18**' |
     seqkit grep -v -p 'Wooster_2022_2' |
     seqkit grep -v -p 'Wooster_2024.10' \
-    > data/COI.fa
-grep -c ">" data/Clean_COI_Sequences.fa data/COI.fa
+    > "$fa"
+grep -c ">" "$fa_init" "$fa"
 
 # Add reference sequences
-ref_fa_init=/fs/ess/PAS0471/jelmer/assist/01_archive/2022-04_ferd_phylo/results/unalign_woutgroups.fa
-seqkit grep -f data/ref_ids.txt "$ref_fa_init" > data/COI_refs.fa
-cat data/COI.fa data/COI_refs.fa > data/COI_withrefs.fa
+seqkit grep -f "$ref_ids" "$ref_fa_init" > "$outdir"/COI_refs.fa
+cat "$fa" "$outdir"/COI_refs.fa > data/COI_withrefs.fa
 
-# Align the sequences 
-bash mcic-scripts/align/align_fa.sh -i data/COI.fa -o results/aligned
-bash mcic-scripts/align/align_fa.sh -i data/COI_withrefs.fa -o results/aligned
+# Align the sequences (with and without reference sequences)
+bash mcic-scripts/align/align_fa.sh -i "$fa" -o "$outdir"/aligned
+bash mcic-scripts/align/align_fa.sh -i data/COI_withrefs.fa -o "$outdir"/aligned
 
-# Clip the alignment with ClipKit
-sbatch mcic-scripts/align/clipkit.sh -i results/aligned/COI_aln.fa -o results/clipkit
-sbatch mcic-scripts/align/clipkit.sh -i results/aligned/COI_withrefs_aln.fa -o results/clipkit
+# Clip the alignment with ClipKit (with and without reference sequences)
+sbatch mcic-scripts/align/clipkit.sh \
+    -i "$outdir"/aligned/COI_aln.fa -o "$outdir"/clipkit
+sbatch mcic-scripts/align/clipkit.sh \
+    -i "$outdir"/aligned/COI_withrefs_aln.fa -o "$outdir"/clipkit
 
 # Make a tree with IQ-Tree
-sbatch -t30 mcic-scripts/trees/iqtree.sh \
-    --infile results/clipkit/COI_withrefs_aln.fa \
-    --outdir results/iqtree \
-    --root "HQ944971.1" \
+sbatch mcic-scripts/trees/iqtree.sh \
+    --infile "$outdir"/clipkit/COI_withrefs_aln.fa \
+    --outdir "$outdir"/iqtree \
+    --root "$outgroup" \
     --nboot 1000 \
-    --opts "--msub mitochondrial"
-# results/iqtree/COI_withrefs_aln.treefile
+    --more_opts "--msub mitochondrial"
+
+
+# ==============================================================================
+#                               NAD4
+# ==============================================================================
+# Define input files
+fa_aln=data/WBNAD4Aligned.fasta
+
+# Define output files
+outdir=results/NAD4 && mkdir -p "$outdir"
+
+# Clip the alignment with ClipKit
+sbatch mcic-scripts/align/clipkit.sh -i "$fa_aln" -o "$outdir"/clipkit
+
+# Make a tree with IQ-Tree
+sbatch mcic-scripts/trees/iqtree.sh \
+    --infile "$outdir"/clipkit/WBNAD4Aligned.fasta \
+    --outdir "$outdir"/iqtree \
+    --nboot 1000 \
+    --more_opts "--msub mitochondrial"
+
+#--root "$outgroup" \
